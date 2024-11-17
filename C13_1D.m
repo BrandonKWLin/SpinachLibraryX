530f17d191357bf6de1ec26bd8fa2fcc519f9fc1
Brandon Lin <168325202+BrandonKWLin@users.noreply.github.com>
2024-11-17 02:23:13 -0500

    Add files via upload

---
+++ C13_1D.m
@@ -0,0 +1,74 @@
+% 1H NMR spectrum of rotenone using T1/T2 relaxation model,
+% magnetic parameters from:
+% 
+%         http://dx.doi.org/10.1002/jhet.5570250160
+%
+% Calculation time: seconds
+%%
+% matthew.krzystyniak@oerc.ox.ac.uk
+% i.kuprov@soton.ac.uk
+
+% *Default Spinach File*
+% This file is included as a backup in case you want to revert to the original settings after 
+% testing the updated Spinach files. It can also serve as a replacement if the default file 
+% from the Spinach package is lost for any reason.
+
+function C13_1D()
+
+% Isotopes
+sys.isotopes={'13C','13C','13C'};
+% Magnetic induction
+sys.magnet=21.1;
+
+% Chemical shifts
+inter.zeeman.scalar={10.0 40 70};
+
+% Scalar couplings
+inter.coupling.scalar{1,2}=50; 
+inter.coupling.scalar{2,3}=55; 
+inter.coupling.scalar{1,3}=0.0; 
+inter.coupling.scalar{3,3}=0; 
+% Relaxation model
+inter.relaxation={'t1_t2'};
+inter.rlx_keep='diagonal';
+inter.r1_rates=num2cell(1.0*ones(1,3));
+inter.r2_rates=num2cell(3.0*ones(1,3));
+inter.equilibrium='zero';
+
+% Basis set
+bas.formalism='sphten-liouv';
+bas.approximation='IK-2';
+bas.connectivity='scalar_couplings';
+bas.space_level=1;
+%bas.sym_group={'S3','S3','S3'};
+%bas.sym_spins={[14 15 16],[17 18 19],[20 21 22]};
+
+% Spinach housekeeping
+spin_system=create(sys,inter);
+spin_system=basis(spin_system,bas);
+
+% Sequence parameters
+parameters.spins={'13C'};
+parameters.rho0=state(spin_system,'L+','13C','cheap');
+parameters.coil=state(spin_system,'L+','13C','cheap');
+parameters.decouple={};
+parameters.offset=10000;
+parameters.sweep=25000;
+parameters.npoints=8192;
+parameters.zerofill=16536;
+parameters.axis_units='ppm';
+parameters.invert_axis=1;
+
+% Simulation
+fid=liquid(spin_system,@acquire,parameters,'nmr');
+
+% Apodization
+fid=apodization(fid,'gaussian-1d',10);
+
+% Fourier transform
+spectrum=fftshift(fft(fid,parameters.zerofill));
+
+% Plotting
+figure(); plot_1d(spin_system,real(spectrum),parameters);
+
+end
